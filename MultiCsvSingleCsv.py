f8d725a4c78000dbe5519533a8ae57a14030dd76
Dillon Glasser <drizzt5@users.noreply.github.com>
2017-02-12 19:39:05 -0800

    Update MultiCsvSingleCsv.py

--- MultiCsvSingleCsv.py
+++ MultiCsvSingleCsv.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 #
+#
+#
+# 
+# ---------------------------------------------------------------------------
+# Looking back at this a month later...
+#
+# Actually, this program is redundant and probably
+# every way that I've used this could be accomplished 
+# by simply using cat. 
+# 
+# Example:
+# (in a directory with just csv files)
+# cat *.csv > [filename.csv]
+# This would take all the csv files in some directory, pump them into [filename.csv]
+# and probably do a quicker job of it.
+#
+# And if I needed to get csv files in other directories into my file, 
+# cat *.csv >> /path/to/filename.csv
+# '>>' should just append to the end of the file.
+#
+# Dang, but I am learning so it's cool.
+# ------------------------------------------------------------------------------
+#
+#
+#
+#
+#
 # Program: Take some csv files and spit out a single csv
 # Author: Dillon Glasser <dpglasse@nps.edu>
 # 
